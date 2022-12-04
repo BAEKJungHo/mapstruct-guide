2e3f883854beeef6cdbe49341a1d5d207ce16d3b
jungho.baek <jungho.baek@42dot.ai>
2022-12-04 17:48:24 +0900

    Mapstruct Test

---
+++ mapstruct/src/main/java/com/baek/mapstruct/mapper/OrderMapper.java
@@ -0,0 +1,16 @@
+package com.baek.mapstruct.mapper;
+
+import com.baek.mapstruct.dto.OrderDto;
+import com.baek.mapstruct.entity.OrderInfo;
+import org.mapstruct.*;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface OrderMapper {
+
+    @Mappings(value = @Mapping(source = "orderedAt", target = "orderedAt", dateFormat = "yyyy-MM-dd HH:mm:ss"))
+    OrderDto.Main of(OrderInfo.Main mainResult);
+}
